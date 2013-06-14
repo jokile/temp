bc2d16d3ccd8ad1988814b7513ecde2d9b32d036
jokile <aleksandr@PowerMaster.local>
2013-06-14 21:03:14 +0400

    update

---
+++ obj_c_project/obj_c_project/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  obj_c_project
+//
+//  Created by Александр on 14.06.13.
+//  Copyright (c) 2013 Александр. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool
+    {
+        
+                
+    }
+    
+    return 0;
+}
+
